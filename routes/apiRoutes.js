2fd59f298d4d6976424ed0635c7e16f708dec6d4
Kp <kp_1@outlook.com>
2021-04-08 05:26:41 +0400

    Updates to API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,34 @@
-const fs = require('fs');
-const noteDisplay = require('../Develop/public/assets/js/index');
+
+const noteDisplay = require('../public/assets/js/index');
 
 //Routing
 module.exports = (app) => {
 
-//Create note list
+// API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
 
-let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+app.get('/api/notes', (req, res) => res.json(noteDisplay));
 
 //.sendFile function to transfer the file at the given path and sets the Content-Type response HTTP
 //route to db.json file and save to json
-app.post('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,'/db/db.json'));
+
+app.post('api/notes', (req, res) => {
+    //Create note list
+    const newNote = req.body;
+    const noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    
+    // res.sendFile(path.join(__dirname,'/db/db.json'));
+    noteList.push(newNote);
 });
 
+app.get("/api/notes/:id", (req,res) => {
+    // display json for the notes array 
+    res.json(notes[req.params.id]);
+});
 
 //POST request for a new note
 app.post('api/notes', (req, res) => {
-    let newNote = req.body;
-    notes.push(newNote);
-    updateDb();
+    notes.push(noteList);
 });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,23 @@
 const path = require('path');
 
+// Check if this is required
+const htmlData = require('../routes/htmlRoutes')
+
 module.exports = (app) => {
 //route to homepage
                 //DOES THIS ORDER MATTER?
-app.get('/html', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
                 
 //route to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+//route to read the 'db.json' file and return as JSON
+app.get('api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
 };
